5dc61a79b20b45e131f1d888fc084ce39eb03c23
Leonardo Souza <leonardoDevSantos@gmail.com>
2024-05-09 10:45:17 -0300

    Incluindo a primeira rota

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,15 @@
 import express from 'express'
 import { sequelize } from './database'
 import { adminJsRouter, adminjs } from './adminjs'
+import { router } from './router'
 
 const app = express()
 
 app.use(express.static('public'))
 
 app.use(adminjs.options.rootPath, adminJsRouter)
+
+app.use(router)
 
 const PORT = process.env.PORT || 3000
 

68a192a2468e05312a1794e9a7b944e10ab02299
Leonardo Souza <leonardoDevSantos@gmail.com>
2024-05-10 11:40:09 -0300

    Buscando por curso

--- src/services/courseService.ts
+++ src/services/courseService.ts
@@ -1,3 +1,4 @@
+import { Op } from "sequelize"
 import { Course } from "../models"
 
 export const courseServices = {
@@ -51,5 +52,32 @@ export const courseServices = {
         })
 
         return courses
+    },
+
+    findByName: async(name:string, page:number, perPage:number) => {
+        const offset = (page - 1) * perPage
+
+        const {count, rows} = await Course.findAndCountAll({
+            attributes: [
+                'id',
+                'name',
+                'synopsis',
+                ['thumbnail_url', 'thumbnailUrl']
+            ],
+            where: {
+                name: {
+                    [Op.iLike]: `%${name}%`
+                }
+            },
+            limit: perPage,
+            offset
+        })
+
+        return {
+            courses: rows,
+            page,
+            perPage,
+            total: count
+        }
     }
 }

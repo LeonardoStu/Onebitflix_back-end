b7409802deb090a0b074924f3dd85a29bbd45742
Leonardo Souza <leonardoDevSantos@gmail.com>
2024-05-10 10:56:53 -0300

    Obtendo 3 cursos em destaque

--- src/controllers/coursesController.ts
+++ src/controllers/coursesController.ts
@@ -2,6 +2,20 @@ import { Request, Response } from "express";
 import { courseServices } from "../services/courseService";
 
 export const coursesController = {
+    // GET /courses/featured
+    featured: async (req: Request, res: Response) => {
+
+        try {
+           const featuredCourses = await courseServices.getRandomFeaturedCourses()
+            return res.json(featuredCourses)
+        } catch (err) {
+            if(err instanceof Error){
+                return res.status(400).json({message: err.message})
+            }
+        }
+    },
+
+    // GET /courses/:id
     show: async (req: Request, res: Response) => {
         const { id } = req.params
 

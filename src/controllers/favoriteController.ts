bb445ae442e2c409d23feeb8ebb59805a417c08e
Leonardo Souza <leonardoDevSantos@gmail.com>
2024-05-15 23:39:16 -0300

    Obtendo os cursos favoritos

--- src/controllers/favoriteController.ts
+++ src/controllers/favoriteController.ts
@@ -3,18 +3,32 @@ import { AuthenticatedRequest } from "../middlewares/auth";
 import { favoriteService } from '../services/favoriteServices'
 
 export const favoritesController = {
- // POST /favorites
-  save: async (req: AuthenticatedRequest, res: Response) => {
-    const userId = req.user!.id
-    const { courseId } = req.body
+    // GET /favorites
+    index: async (req: AuthenticatedRequest, res: Response) => {
+        const userId = req.user!.id
 
-    try {
-      const favorite = await favoriteService.create(userId, courseId)
-      return res.status(201).json(favorite)
-    } catch (err) {
-      if (err instanceof Error) {
-        return res.status(400).json({ message: err.message })
-      }
+        try {
+            const favorites = await favoriteService.findByUserId(userId)
+            return res.json(favorites)
+        } catch (err) {
+            if (err instanceof Error) {
+                return res.status(400).json({ message: err.message })
+            }
+        }
+    },
+
+    // POST /favorites
+    save: async (req: AuthenticatedRequest, res: Response) => {
+        const userId = req.user!.id
+        const { courseId } = req.body
+
+        try {
+            const favorite = await favoriteService.create(userId, courseId)
+            return res.status(201).json(favorite)
+        } catch (err) {
+            if (err instanceof Error) {
+                return res.status(400).json({ message: err.message })
+            }
+        }
     }
-  }
 }
